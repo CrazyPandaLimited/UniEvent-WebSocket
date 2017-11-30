e0122a870205e2996585cd1511aa9253873f2939
g.smorkalov <devnull@localhost>
2017-11-30 19:25:03 +0300

    wconnection config fixes and XS for it

--- plus/Panda-WebSocket-Server/src/xs/websocket/server/XSServer.h
+++ plus/Panda-WebSocket-Server/src/xs/websocket/server/XSServer.h
@@ -6,6 +6,8 @@
 namespace xs { namespace websocket { namespace server {
 
 using panda::websocket::server::Server;
+using panda::websocket::server::ServerConfig;
+using panda::websocket::server::Location;
 using panda::websocket::server::Connection;
 
 class XSServer : public Server, public xs::XSBackref {
@@ -15,6 +17,9 @@ public:
     using Server::Server;
 
     ConnectionSP new_connection(uint64_t id) override;
+
+    static Location make_location(HV* hvloc);
+    static ServerConfig make_server_config(HV* hvcfg);
 };
 
 }}}
