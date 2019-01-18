68ba2a4356200c2c9cdb8b55156e9032e106985e
i.baydakov <devnull@localhost>
2019-01-18 20:31:02 +0300

    [SRV-1249] log deflate status on connection accept(VERBOSE_DEBUG)

--- plus/UniEvent-WebSocket/src/panda/unievent/websocket/ServerConnection.cc
+++ plus/UniEvent-WebSocket/src/panda/unievent/websocket/ServerConnection.cc
@@ -55,6 +55,21 @@ void ServerConnection::send_accept_error (HTTPResponse* res) {
 
 void ServerConnection::send_accept_response (ConnectResponse* res) {
     write(parser.accept_response(res));
+    auto using_deflate = parser.is_deflate_active();
+    panda_log_debug("websocket::ServerConnection " << id() << " has been accepted, "
+                    << "deflate is " << (using_deflate ? "on" : "off"));
+
+    if (Log::should_log(logger::VERBOSE_DEBUG, _panda_code_point_)){
+        Log logger = Log(_panda_code_point_, logger::VERBOSE_DEBUG);
+        auto deflate_cfg = parser.effective_deflate_config();
+        logger << "websocket::ServerConnection " << id() << " agreed deflate settings"
+               << ": server_max_window_bits = " << (int)deflate_cfg->server_max_window_bits
+               << ", client_max_window_bits = " << (int)deflate_cfg->client_max_window_bits
+               << ", server_no_context_takeover = " << deflate_cfg->server_no_context_takeover
+               << ", client_no_context_takeover = " << deflate_cfg->client_no_context_takeover;
+    }
+
+
 }
 
 void ServerConnection::close (uint16_t code, const string& payload) {
