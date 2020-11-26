74a521247a665c0827fcd6a568dbec4e2d9d376a
Ilya Rumeev <i.rumeev@crazypanda.ru>
2020-11-26 14:41:41 +0500

    Revert previous commit; This reverts commit b3a77154d728db5005abfc55f2fdaa6d914f31f6.

--- plus/UniEvent-WebSocket/src/panda/unievent/websocket/Listener.cc
+++ plus/UniEvent-WebSocket/src/panda/unievent/websocket/Listener.cc
@@ -10,8 +10,12 @@ void Listener::run () {
     panda_log_notice("Listener[run]: listening " << (_location.ssl_ctx ? "wss://" : "ws://") << _location.host << ":" << _location.port
                    << ", backlog:" << _location.backlog << ", wbuf:" << send_buffer_size() << ", rbuf:" << recv_buffer_size());
     if (_location.reuse_port) {
+        #ifdef _WIN32
+        panda_log_warning("ignored reuse_port configuration parameter: not supported on windows");
+        #else
         int on = 1;
         setsockopt(SOL_SOCKET, SO_REUSEPORT, &on, sizeof(on));
+        #endif
     }
     bind(_location.host, _location.port);
     listen(_location.backlog);
